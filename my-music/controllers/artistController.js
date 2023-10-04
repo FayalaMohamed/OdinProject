8bb050df7d16326000ac511a59809f63ea5eaaff
FayalaMohamed <fayalla.mohamed@gmail.com>
2023-10-04 21:42:17 +0200

    my-music : artist part finished

--- my-music/controllers/artistController.js
+++ my-music/controllers/artistController.js
@@ -142,42 +142,142 @@ exports.artist_create_post = [
 ]
 
 exports.artist_delete_get = asyncHandler(async (req, res, next) => {
-    const artists = await Artist.find({}, "professional_name birth_name")
-        .sort({ professional_name: 1 })
-        .exec();
+    const [artist, artist_albums] = await Promise.all([
+        Artist.findById(req.params.id).exec(),
+        Album.find({ artists: { $in: req.params.id } }).sort({ name: 1 }).exec(),
+    ]);
 
-    res.render('artist_list', {
-        title: "Artist list",
-        artist_list: artists,
-    })
+    if (artist === null) {
+        res.redirect('/catalog/artists');
+    } else {
+        res.render("artist_delete", {
+            title: "Delete artist",
+            artist,
+            artist_albums,
+        });
+    }
+    
 });
 exports.artist_delete_post = asyncHandler(async (req, res, next) => {
-    const artists = await Artist.find({}, "professional_name birth_name")
-        .sort({ professional_name: 1 })
-        .exec();
-
-    res.render('artist_list', {
-        title: "Artist list",
-        artist_list: artists,
-    })
+    const [artist, artist_albums] = await Promise.all([
+        Artist.findById(req.params.id).exec(),
+        Album.find({ artists: { $in: req.params.id } }).sort({ name: 1 }).exec(),
+    ]);
+    
+    if (artist_albums.length > 0) {
+        res.render("artist_delete", {
+            title: "Delete artist",
+            artist,
+            artist_albums,
+        });
+    } else {
+        await Artist.findByIdAndRemove(req.body.artistid);
+        res.redirect('/catalog/artists')
+    }
 });
+
 exports.artist_update_get = asyncHandler(async (req, res, next) => {
-    const artists = await Artist.find({}, "professional_name birth_name")
-        .sort({ professional_name: 1 })
-        .exec();
+    const [artist, allGenres] = await Promise.all([
+        Artist.findById(req.params.id).exec(),
+        Genre.find({}).exec(),
+    ]);
 
-    res.render('artist_list', {
-        title: "Artist list",
-        artist_list: artists,
+    if (artist === null) {
+        const err = new Error("Artist not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    for (const genre of allGenres) {
+        for (const artist_genre of artist.genres) {
+            if (genre._id.toString() === artist_genre._id.toString()) {
+                genre.checked = "true";
+            }
+        }
+    }
+
+    res.render('artist_form', {
+        title: "Update artist",
+        genres: allGenres,
+        artist: artist,
     })
 });
-exports.artist_update_post = asyncHandler(async (req, res, next) => {
-    const artists = await Artist.find({}, "professional_name birth_name")
-        .sort({ professional_name: 1 })
-        .exec();
 
-    res.render('artist_list', {
-        title: "Artist list",
-        artist_list: artists,
-    })
-});
+exports.artist_update_post = [
+    (req, res, next) => {
+        if (!(req.body.genres instanceof Array)) {
+            if (typeof req.body.genres === "undefined") req.body.genres = [];
+            else req.body.genres = new Array(req.body.genres);
+        }
+        next();
+    },
+
+    (req, rep, next) => {
+        if (typeof req.body.occupations === 'string') {
+            req.body.occupations = req.body.occupations.split('\n').map((occupation) => occupation.trim());
+        } else if (!(req.body.occupations instanceof Array)) {
+            req.body.occupations = [];
+        }
+        next();
+    },
+
+    body("birth_name")
+        .trim()
+        .isLength({ min: 1 })
+        .escape()
+        .withMessage("Birth name must not be empty"),
+    body("professional_name", "Professional name must not be empty")
+        .trim()
+        .isLength({ min: 1 })
+        .escape(),
+    body("date_of_birth", "Invalid date of birth")
+        .optional({ checkFalsy: true })
+        .isISO8601()
+        .toDate(),
+    body("date_of_death", "Invalid date of death")
+        .optional({ checkFalsy: true })
+        .isISO8601()
+        .toDate(),
+    body("place_of_birth")
+        .trim()
+        .escape(),
+    body("genres.*").escape(),
+    body("occupations.*").escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const artist = new Artist({
+            birth_name: req.body.birth_name,
+            professional_name: req.body.professional_name,
+            date_of_birth: req.body.date_of_birth,
+            date_of_death: req.body.date_of_death,
+            place_of_birth: req.body.place_of_birth,
+            occupations: req.body.occupations,
+            genres: req.body.genres,
+            _id: req.params.id,
+        });
+
+        if (!errors.isEmpty()) {
+            const allGenres = await Genre.find({}).exec();
+
+            for (const genre of allGenres) {
+                if (artist.genres.indexOf(genre._id) > -1) {
+                    genre.checked = "true";
+                }
+            }
+
+            res.render('artist_form', {
+                title: "Update artist",
+                genres: allGenres,
+                artist: artist,
+                errors: errors.array(),
+            })
+        }
+        else {
+            const theArtist = await Artist.findByIdAndUpdate(req.params.id, artist, {});
+            res.redirect(theArtist.url);
+        }
+    }),
+
+]
